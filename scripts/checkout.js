0ea8e02af515e58b02c8f976d3e130218143adc6
anubhavtya6i <anutyagi2602@gmail.com>
2026-06-29 21:49:13 +0530

    Load Cart from Backend and Promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,9 +3,49 @@ import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
+Promise.all([
+    new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('Value1'); 
+    });
+}) ,
+    new Promise((resolve) => {
+    loadCart(() => {
+        resolve();
+    });
+})
 
-loadProducts(() => {
+]).then((values) => {
+    console.log(values)
+     renderOrderSummary();
+    renderPaymentSummary();
+});
+
+/* 
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('Value1'); 
+    });
+
+}).then((value) => {
+    console.log(value);
+return new Promise((resolve) => {
+    loadCart(() => {
+        resolve();
+    });
+})
+
+}).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
+});
+ */
+/* loadProducts( () => {
+    loadCart(() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    });
 })
+ */
