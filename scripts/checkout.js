f249058fe9478d3b520612e7ec18239cca2d376f
anubhavtya6i <anutyagi2602@gmail.com>
2026-07-01 18:02:47 +0530

    Async and Await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,21 @@ import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
+async function loadPage(){
+
+    await loadProductsFetch();
+
+    await new Promise((resolve) => {
+    loadCart(() => {
+        resolve();
+    });
+})
+    renderOrderSummary();
+    renderPaymentSummary();
+
+}
+loadPage();
+/* 
 Promise.all([
     loadProductsFetch() ,
     new Promise((resolve) => {
@@ -17,7 +32,7 @@ Promise.all([
     console.log(values)
      renderOrderSummary();
     renderPaymentSummary();
-});
+}); */
 
 /* 
 new Promise((resolve) => {
